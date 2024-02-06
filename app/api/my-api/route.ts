7c1a41edee229d1c1e9ec5ee865b5fc240c14e0a
tienqtran <tienqtran@kms-healthcare.com>
2024-02-06 20:54:16 +0700

    changed auth0 route

--- app/api/my-api/route.ts
+++ app/api/my-api/route.ts
@@ -1,15 +1,8 @@
 import { NextResponse } from 'next/server';
-import { getAccessToken } from '@auth0/nextjs-auth0';
-import { getSession } from '@auth0/nextjs-auth0';
 
 export async function GET() {
-  const { accessToken } = await getAccessToken();
-  const session = await getSession();
   return NextResponse.json({
     foo: 'bar',
-    acc: accessToken,
-    ssAc: session?.accessToken,
-    ssRf: session?.refreshToken,
-    test: 123
+    aw: 123
   });
 }
